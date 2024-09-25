1d4409aa29c80876ba7eae5bdbe2735dd6cdf8e1
Sharan-Muthu-Krishna <sharanmuthukrishna@gmail.com>
2024-09-25 23:37:33 +0530

    2 pages

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+router.get('/auth/fail', function (req, res, next) {
+    res.render('error');
+});
+
+router.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/auth/fail' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
